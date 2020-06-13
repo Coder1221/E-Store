cec508d9cc5bd909ed9622c423236da95bb7ca18
Coder1221 <21100328@lums.edu.pk>
2020-06-13 18:29:09 +0500

    ADDED ALOT

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import {Switch , Route} from 'react-router-dom';
+import Navbar from './components/navbar';
+import List from './components/list';
+import Details from './components/details';
+import Cart from './components/cart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+       <Navbar/>
+       <Switch>
+        <Route exact path='/' component={List}/>
+        <Route path="/cart" component={Cart} />
+        <Route path="/details" component={Details} />
+       </Switch>
+      </React.Fragment>
+    );
+  }
 }
-
 export default App;
